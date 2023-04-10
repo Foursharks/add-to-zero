b038c3a0fbf00965a1c4baae5b59cc4950973d0b
Foursharks <69390429+Foursharks@users.noreply.github.com>
2023-04-10 15:47:54 -0400

    Initial commit and solution

---
+++ addToZero.js
@@ -0,0 +1,28 @@
+// Given an array of numbers, console.log true if any two numbers in the array add to zero, otherwise log false.
+
+// For example:
+
+// [1, 4, 11, 2, 37, -4] should log true because 4 and -4 add to 0.
+
+// [0, 21, 33, 6, 0, -9] should log true because 0 and 0 add to 0.
+
+// [0, 1, 2, 3, 4, 5] should log false because no two numbers add to 0.
+
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]; 
+
+// Write your solution below:
+
+
+ for (i=0; i<array.length; i++){
+    let num1 = array[i]; 
+    for(let j=0; j<array.length; j++){
+        let num2 = array[j]; 
+        if(num1 + num2 === 0){
+            console.log(num1, num2, num1 + num2, `true`); 
+        }
+        else {
+            console.log(num1, num2,num1 + num2,`false`); 
+        }
+    }
+ }
